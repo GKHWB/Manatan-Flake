5297f1c383c796113ec0a53ca1d18488ab48e325
GKHWB <kingdomg@tuta.com>
2026-03-04 18:54:48 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Nix flake for the Manatan immersion software";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }@inputs: 
+  let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in
+  {
+    packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        manatan = pkgs.callPackage ./manatan.nix { };
+      in {
+        default = manatan;
+      }
+    );
+  };
+}
---
+++ manatan.nix
@@ -0,0 +1,70 @@
+{ 
+  stdenv,
+  fetchurl,
+  libgcc,
+  wayland,
+  libxkbcommon,
+  buildFHSEnv,
+}:
+
+let
+
+pkg = stdenv.mkDerivation (finalAttrs: {
+  pname = "manatan";
+  version = "3.2.11";
+
+  src = 
+  let
+    selectSystem =
+      attrs:
+      attrs.${stdenv.hostPlatform.system} or (throw "Unsupported system: %{stdenv.hostPlatform.system}");
+    system = selectSystem {
+      x86_64-linux = "amd64";
+      aarch64-linux = "arm64";
+    };
+  in
+    fetchurl {
+      url = "https://github.com/KolbyML/Manatan/releases/download/v${finalAttrs.version}/Manatan-v${finalAttrs.version}-Linux-${system}.tar.gz";
+      sha256 = selectSystem {
+        x86_64-linux = "sha256-L/J7sudvSIh13OkFdCayZveZA8uCJznhe53jmIA9Rvk=";
+        aarch64-linux = "sha256-L21yFOKbkmcYmYsuLzFpNssVVMu9zymSi/2fDISq2VM=";
+      };
+    };
+
+  buildInputs = [
+    libgcc
+  ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    install -m755 -D manatan $out/bin/manatan
+  '';
+
+  runtimeDependencies = [ 
+    wayland 
+    libxkbcommon
+  ];
+
+  meta = {
+    homepage = "https://manatan.com";
+    description = "Seamless immersion language learning for anime, manga, novels on all platforms";
+  };
+});
+
+in
+
+buildFHSEnv {
+  inherit (pkg) pname version;
+
+  runScript = "${pkg.outPath}/bin/manatan";
+
+  targetPkgs = pkgs: with pkgs; [
+    fontconfig
+    wayland
+    libxkbcommon
+    freetype
+    libz
+    libGL
+  ];
+}
