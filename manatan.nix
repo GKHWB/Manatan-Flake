c641d13fe56d064d5adb50518cbd7b35bb65589d
GKHWB <kingdomg@tuta.com>
2026-03-11 19:24:36 -0400

    3.3.1 -> 3.3.2

--- manatan.nix
+++ manatan.nix
@@ -11,7 +11,7 @@ let
 
 pkg = stdenv.mkDerivation (finalAttrs: {
   pname = "manatan";
-  version = "3.3.1";
+  version = "3.3.2";
 
   src = 
   let
@@ -26,8 +26,8 @@ pkg = stdenv.mkDerivation (finalAttrs: {
     fetchurl {
       url = "https://github.com/KolbyML/Manatan/releases/download/v${finalAttrs.version}/Manatan-v${finalAttrs.version}-Linux-${system}.tar.gz";
       sha256 = selectSystem {
-        x86_64-linux = "sha256-I+utP6RjwfQxHaYJPHtvM69oKYE03fUFiFIP1+wK4VI=";
-        aarch64-linux = "sha256-cE1ncIAIwt8kuk/TOe38LFtK3Ei9WoNnzPPr+7WIaWM=";
+        x86_64-linux = "sha256-TYlBbd0Q6mLqTfpDpwcaMUmFuiRF2zVw4joMZJqL8EU=";
+        aarch64-linux = "sha256-nR78xUhJVPt6sAE5Q9QLqN1cjOdpIrGCJ9BrET1rllg=";
       };
     };
 
